60ce1c426c59ee6e1962dc4f6937e7ac8a1e93a5
Michael Kahane <kaham340@newschool.edu>
2016-05-29 13:09:48 -0400

    A header!

---
+++ js/kahane.js
@@ -0,0 +1,39 @@
+// Define our "kahane" object, if not already defined
+if ( kahane === undefined ) { var kahane = {}; }
+
+var kahane = (function($) {
+
+	'use strict';
+
+	var
+
+	// Globally available variables
+	$body = $('body'),
+
+	/* ==========================================================================
+	   hamburger
+	========================================================================== */
+	hamburger = function() {
+		$('.hamburger').click(function(){
+			$(this).toggleClass('open');
+		});
+	},
+
+	/* ==========================================================================
+	   Init
+	========================================================================== */
+
+	init = function() {
+		hamburger();
+	};
+
+	return {
+		init:init
+	};
+
+}(jQuery));
+
+$(function() {
+	'use strict';
+	kahane.init();
+});
