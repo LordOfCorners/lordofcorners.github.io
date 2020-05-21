adaddc9fa6ae9e17b6bbf7a217e7acb4f7e2c6d8
Michael Kahane <mkahane@hugeinc.com>
2020-05-21 02:04:55 -0400

    remove jquery

--- js/kahane.js
+++ js/kahane.js
@@ -1,39 +1,12 @@
-// Define our "kahane" object, if not already defined
-if ( kahane === undefined ) { var kahane = {}; }
-
-var kahane = (function($) {
-
-	'use strict';
-
-	var
-
-	// Globally available variables
-	$body = $('body'),
-
-	/* ==========================================================================
-	   hamburger
-	========================================================================== */
-	hamburger = function() {
-		$('.hamburger').click(function(){
-			$(this).add('#menu, #logo').toggleClass('open');
-		});
-	},
-
-	/* ==========================================================================
-	   Init
-	========================================================================== */
-
-	init = function() {
-		hamburger();
-	};
-
-	return {
-		init:init
-	};
-
-}(jQuery));
-
-$(function() {
-	'use strict';
-	kahane.init();
-});
+document.addEventListener(
+  'click',
+  function (event) {
+    if (
+      !event.target.matches('.hamburger') &&
+      !event.target.matches('.burger-bar')
+    )
+      return;
+    document.body.classList.toggle('open');
+  },
+  false
+);
