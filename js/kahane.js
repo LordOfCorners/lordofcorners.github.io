021dfe52b238771602d4a4acbbc8bbd33f511090
Michael Kahane <mkahane@hugeinc.com>
2020-05-21 11:54:27 -0400

    tabbing accessibility for menu

--- js/kahane.js
+++ js/kahane.js
@@ -1,12 +1,47 @@
-document.addEventListener(
+var body = document.body;
+var logo = document.getElementById('logo');
+var nav = document.getElementById('nav');
+var navItems = document.querySelectorAll('.menu_pages');
+var lastNavItem = navItems[navItems.length - 1];
+var KEYCODE_TAB = 9;
+
+nav.addEventListener(
   'click',
-  function (event) {
-    if (
-      !event.target.matches('.hamburger') &&
-      !event.target.matches('.burger-bar')
-    )
-      return;
-    document.body.classList.toggle('open');
+  function () {
+    event.preventDefault();
+    body.classList.toggle('open');
   },
   false
 );
+
+document.addEventListener('keydown', function (event) {
+  var isTabPressed = event.key === 'Tab' || event.keyCode === KEYCODE_TAB;
+
+  if (!isTabPressed) {
+    return;
+  }
+
+  if (event.shiftKey) {
+    /* shift + tab */ if (document.activeElement === logo) {
+      lastNavItem.focus();
+      event.preventDefault();
+    }
+  } /* tab */ else {
+    if (document.activeElement === lastNavItem) {
+      console.log('test');
+      console.log(logo);
+      window.setTimeout(function () {
+        logo.focus();
+      }, 200);
+
+      event.preventDefault();
+    }
+  }
+});
+
+nav.addEventListener('keyup', function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    nav.click();
+  }
+});
